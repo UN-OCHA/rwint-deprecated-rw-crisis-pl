236370af17b30e9b9fb2b7d506cd311303a21f3a
Jonathan Adams <adams.jonathan@outlook.com>
2015-01-08 09:35:48 -0500

    Timeline: Initial work.

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-	$('body').addClass("TEST");
+
+  var $frame = $('.timeline-widget');
+  var $wrap  = $frame.parent();
+
+  $frame.sly({
+    horizontal: 1,
+    itemNav: 'forceCentered',
+    smart: 1,
+    activateMiddle: 1,
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+    startAt: 0,
+    //scrollBar: $wrap.find('.scrollbar'),
+    scrollBy: 1,
+    speed: 300,
+    elasticBounds: 1,
+    easing: 'easeOutExpo',
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1,
+
+    // Buttons
+    prev: $wrap.find('.prev'),
+    next: $wrap.find('.next')
+  });
+
 });
