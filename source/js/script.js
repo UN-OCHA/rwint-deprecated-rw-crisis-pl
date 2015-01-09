90294256b12d8c03a98ebdd6583090100d5c92f4
Jonathan Adams <adams.jonathan@outlook.com>
2015-01-09 17:38:14 -0500

    timeline: Synced all sliders.

--- source/js/script.js
+++ source/js/script.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
     $sly.reload();
   });
 
+  // Main slider.
   var $sly = new Sly($frame, {
     horizontal: 1,
     itemNav: 'forceCentered',
@@ -42,35 +43,65 @@ $(document).ready(function() {
     next: $wrap.find('.next')
   }).init();
 
-  /*var $slyPager = new Sly($pager, {
+  // Pager.
+  var $slyPager = new Sly('.timeline-widget-pager', {
     horizontal: 1,
-    itemNav: 'centered',
+    itemNav: 'forceCentered',
+    smart: 1,
+    activateMiddle: 1,
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+    startAt: 0,
+    scrollBy: 1,
+    speed: 200,
+    elasticBounds: 1,
+    //easing: 'easeOutExpo',
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1
+  }).init();
+
+  // Dropdowns.
+  var $slyDropdown = new Sly('.timeline-widget--dropdown--container', {
+    itemNav: 'basic',
     smart: 1,
     activateOn: 'click',
     mouseDragging: 1,
     touchDragging: 1,
     releaseSwing: 1,
     startAt: 0,
-    //scrollBar: $wrap.find('.scrollbar'),
     scrollBy: 1,
+    activatePageOn: 'click',
     speed: 300,
     elasticBounds: 1,
-    easing: 'easeOutExpo',
     dragHandle: 1,
     dynamicHandle: 1,
     clickBar: 1
+  }).init();
 
-    // Buttons
-    //prev: $wrap.find('.prev'),
-    //next: $wrap.find('.next')
-  }).init();*/
-
-  $('.timeline-widget-pager--item').click(function(){
-
+  $('.timeline-widget-pager--item, .timeline-widget-dropdown--item').click(function(){
     var $index = $(this).attr('data-slide');
     var $pos = $sly.getPos($index);
     $sly.slideTo($pos.start);
+    var $dropDownPos = $slyDropdown.getPos($index);
+  });
 
-    console.log($index);
+  // Open popup.
+  $('.timeline-widget--dropdown-heading').click(function(){
+    $('.timeline-widget--dropdown--wrapper').toggleClass('open');
+    $slyDropdown.reload();
+  });
+
+  // Update other sliders based on main.
+  $sly.on('moveEnd', function(){
+    $index = $sly.rel.activeItem;
+
+    var $pagerPos = $slyPager.getPos($index);
+    $slyPager.slideTo($pagerPos.center);
+
+    var $dropDownPos = $slyDropdown.getPos($index);
+    $slyDropdown.slideTo($dropDownPos.start);
   });
 });
+
