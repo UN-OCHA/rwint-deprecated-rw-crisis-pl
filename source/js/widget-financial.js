69193f2699dab5a2dba6178a1d599188e8e3b2f9
Mason Wendell <mason@thecodingdesigner.com>
2015-01-26 21:58:24 -0500

    bubbles

--- source/js/widget-financial.js
+++ source/js/widget-financial.js
@@ -1,9 +1,11 @@
-  // Open popup.
-  $('.cluster-bubble').click(function(){
-    $('#detail-overlay').show();
-  });
+// Open popup.
+$('.cluster-bubble').click(function(){
+  console.log('bubble');
+  $('#finance-bubbles #detail-overlay').css('display', 'block');
+});
 
-  // Close popup.
-  $('#detail-overlay').click(function(){
-    $('#detail-overlay').hide();
-  });
+// Close popup.
+$('#finance-bubbles #detail-overlay').click(function(){
+  console.log('close bubble');
+  $('#finance-bubbles #detail-overlay').css('display', 'none');
+});
