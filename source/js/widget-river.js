9241af129e691ac98b2d0f53e5967967368016a1
Mason Wendell <mason@thecodingdesigner.com>
2015-01-26 21:36:17 -0500

    filter design revisions

--- source/js/widget-river.js
+++ source/js/widget-river.js
@@ -1,6 +1,14 @@
   // Open popup.
-  $('.widget-river--results--item, .close').click(function(){
-    $('.widget-river--filters').toggleClass('open');
+  $('.widget-river--results--item').click(function(){
+    var item_name  = $(this).attr('data-type-name'),
+        item_class = $(this).attr('data-type-class')
+    $('.widget-river--filters').removeClass (function (index, css) {
+      return (css.match (/\bresults--item--\S+/g) || []).join(' ');
+    });
+    $('.widget-river--filters')
+        .addClass('open')
+        .addClass(item_class)
+        .find('.widget-river--filters--title .tab').text(item_name);
   });
 
   // Close popup.
