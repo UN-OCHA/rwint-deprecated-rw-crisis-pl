3e21c511f63d0eee1d5780bd8dcd8874bb7e6d09
Mason Wendell <mason@thecodingdesigner.com>
2015-01-26 21:51:02 -0500

    financial bubble pop up

---
+++ source/js/widget-financial.js
@@ -0,0 +1,9 @@
+  // Open popup.
+  $('.cluster-bubble').click(function(){
+    $('#detail-overlay').show();
+  });
+
+  // Close popup.
+  $('#detail-overlay').click(function(){
+    $('#detail-overlay').hide();
+  });
