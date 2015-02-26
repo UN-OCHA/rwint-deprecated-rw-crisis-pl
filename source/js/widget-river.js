d6790595b14f133ece95d58462c5b616d5e3c16a
Mason Wendell <mason@thecodingdesigner.com>
2015-02-26 10:13:23 -0500

    new river template scss files

--- source/js/widget-river.js
+++ source/js/widget-river.js
@@ -1,17 +1,17 @@
   // Open popup.
-  $('.widget-river--results--item').click(function(){
+  $('.widget-river-graph--results--item').click(function(){
     var item_name  = $(this).attr('data-type-name'),
         item_class = $(this).attr('data-type-class')
-    $('.widget-river--filters').removeClass (function (index, css) {
+    $('.widget-river-graph--filters').removeClass (function (index, css) {
       return (css.match (/\bresults--item--\S+/g) || []).join(' ');
     });
-    $('.widget-river--filters')
+    $('.widget-river-graph--filters')
         .addClass('open')
         .addClass(item_class)
-        .find('.widget-river--filters--title .tab').text(item_name);
+        .find('.widget-river-graph--filters--title .tab').text(item_name);
   });
 
   // Close popup.
   $('.close').click(function(){
-    $('.widget-river--filters').removeClass('open');
+    $('.widget-river-graph--filters').removeClass('open');
   });
