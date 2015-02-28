0108668d98ae08fb875e882c60f6ad2a0b28f4a5
Mason Wendell <mason@thecodingdesigner.com>
2015-02-28 14:07:08 -0500

    river styling

--- source/js/widget-river.js
+++ source/js/widget-river.js
@@ -1,17 +1,6 @@
-  // Open popup.
-  $('.widget-river-graph--results--item').click(function(){
-    var item_name  = $(this).attr('data-type-name'),
-        item_class = $(this).attr('data-type-class')
-    $('.widget-river-graph--filters').removeClass (function (index, css) {
-      return (css.match (/\bresults--item--\S+/g) || []).join(' ');
-    });
-    $('.widget-river-graph--filters')
-        .addClass('open')
-        .addClass(item_class)
-        .find('.widget-river-graph--filters--title .tab').text(item_name);
-  });
-
-  // Close popup.
-  $('.close').click(function(){
-    $('.widget-river-graph--filters').removeClass('open');
-  });
+$(document).ready(function () {
+  $width = $(window).width();
+  if ($width > 640) {
+    $('.widget-river--filters input:first-of-type').click();
+  }
+});
